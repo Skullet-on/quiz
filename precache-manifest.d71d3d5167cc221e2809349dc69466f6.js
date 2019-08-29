ee2135d151ee77728047473080acb0f349a07ca7
unknown <skullet-on@mail.ru>
2019-08-29 16:20:56 +0300

    Updates

---
+++ precache-manifest.d71d3d5167cc221e2809349dc69466f6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9c120817035ea16d2539b0f40b16094e",
+    "url": "/quiz/index.html"
+  },
+  {
+    "revision": "7db7b8a18066ae0e6df3",
+    "url": "/quiz/static/css/main.b6701081.chunk.css"
+  },
+  {
+    "revision": "38786fbf8fc1f00a6ffa",
+    "url": "/quiz/static/js/2.86785fdc.chunk.js"
+  },
+  {
+    "revision": "7db7b8a18066ae0e6df3",
+    "url": "/quiz/static/js/main.371f5083.chunk.js"
+  },
+  {
+    "revision": "c858557ecb6f8c2b7535",
+    "url": "/quiz/static/js/runtime~main.f6df2435.js"
+  }
+]);
